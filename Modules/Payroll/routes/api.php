8c2a0a33d6e83f829c14c0023b4958d875441e91
m.karimi <mohsen.mkn2@gmail.com>
2026-08-11 13:12:22 +0330

    تغیر پرمیژن ها

--- Modules/Payroll/routes/api.php
+++ Modules/Payroll/routes/api.php
@@ -10,20 +10,20 @@ Route::middleware(['auth:sanctum'])->prefix('v1')->name('api.')->group(function 
 
     Route::prefix('/payroll')->group(function () {
         // لیست ماه‌های موجود
-        Route::get('/months', [PayslipController::class, 'months'])->middleware('permission:Payroll.view');
+        Route::get('/months', [PayslipController::class, 'months'])->middleware('Payroll.view');
 
         // فیش حقوقی ماه خاص
         Route::get('/payslip/{yearMonth}', [PayslipController::class, 'show'])
-            ->where('yearMonth', '[0-9]{6}')->middleware('permission:Payroll.view');
+            ->where('yearMonth', '[0-9]{6}')->middleware('Payroll.view');
 
         // دانلود PDF فیش حقوقی
         Route::get('/payslip/{yearMonth}/pdf', [PayslipController::class, 'downloadPdf'])
-            ->where('yearMonth', '[0-9]{6}')->middleware('permission:Payroll.view');
+            ->where('yearMonth', '[0-9]{6}')->middleware('Payroll.view');
 
         // مسیرهای مدیران/HR
-            Route::get('/employees', [PayslipController::class, 'employees'])->middleware('permission:AdminPayroll.view');
-            Route::get('/employees/{employeeId}/payslip/{yearMonth}', [PayslipController::class, 'showForEmployee'])->middleware('permission:AdminPayroll.view');
+            Route::get('/employees', [PayslipController::class, 'employees'])->middleware('AdminPayroll.view');
+            Route::get('/employees/{employeeId}/payslip/{yearMonth}', [PayslipController::class, 'showForEmployee'])->middleware('AdminPayroll.view');
             Route::get('/employees/{employeeId}/payslip/{yearMonth}/pdf', [PayslipController::class, 'downloadEmployeePdf'])
-                 ->where('yearMonth', '[0-9]{6}')->middleware('permission:AdminPayroll.view');
+                 ->where('yearMonth', '[0-9]{6}')->middleware('AdminPayroll.view');
     });
 });
